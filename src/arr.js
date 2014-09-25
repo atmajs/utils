bb43a73daf1dff818d4739d65c1aaa88e2f3cef7
Alex Kit <source.kit@gmail.com>
2014-09-25 04:09:15 +0200

    + arr: indexOf/each

--- src/arr.js
+++ src/arr.js
@@ -1,9 +1,22 @@
-var arr_remove;
+var arr_remove,
+	arr_each,
+	arr_indexOf,
+	arr_contains;
 (function(){
 	arr_remove = function(array, x){
 		var i = array.indexOf(x);
 		if (i === -1) 
-			return;
+			return false;
 		array.splice(i, 1);
+		return true;
+	};
+	arr_each = function(arr, fn, ctx){
+		arr.forEach(fn, ctx);
+	};
+	arr_indexOf = function(arr, x){
+		return arr.indexOf(x);
+	};
+	arr_contains = function(arr, x){
+		return arr.indexOf(x) !== -1;
 	};
 }());
--- src/refs.js
+++ src/refs.js
@@ -1,3 +1,6 @@
 var _Array_slice = Array.prototype.slice,
 	_Array_splice = Array.prototype.splice,
-	_Array_indexOf = Array.prototype.indexOf;
+	_Array_indexOf = Array.prototype.indexOf,
+	
+	_Object_create = obj_create,
+	_Object_hasOwnProp = Object.hasOwnProperty;
