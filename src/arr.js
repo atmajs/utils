eb280843443ee83eded75c24dbf50d654681a18a
Alex Kit <source.kit@gmail.com>
2015-02-21 16:59:37 +0100

    + arr_pushMany

--- src/arr.js
+++ src/arr.js
@@ -1,7 +1,8 @@
 var arr_remove,
 	arr_each,
 	arr_indexOf,
-	arr_contains;
+	arr_contains,
+	arr_pushMany;
 (function(){
 	arr_remove = function(array, x){
 		var i = array.indexOf(x);
@@ -18,5 +19,17 @@ var arr_remove,
 	};
 	arr_contains = function(arr, x){
 		return arr.indexOf(x) !== -1;
+	};
+	arr_pushMany = function(arr, arrSource){
+		if (arrSource == null || arr == null || arr === arrSource) 
+			return;
+		
+		var il = arr.length,
+			jl = arrSource.length,
+			j = -1
+			;
+		while( ++j < jl ){
+			arr[il + j] = arrSource[j];
+		}
 	};
 }());
