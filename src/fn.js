788343218164a001d73142281c19206ce871a892
Alex Kit <source.kit@gmail.com>
2015-06-09 16:59:11 +0200

    add (fn) pattern delegate

--- src/fn.js
+++ src/fn.js
@@ -1,6 +1,7 @@
 var fn_proxy,
 	fn_apply,
-	fn_doNothing;
+	fn_doNothing,
+	fn_patternDelegate;
 (function(){
 	fn_proxy = function(fn, ctx) {
 		return function(){
@@ -27,4 +28,36 @@ var fn_proxy,
 	fn_doNothing = function(){
 		return false;
 	};
+	
+	fn_patternDelegate = function(definitions, ctx){
+		var imax = definitions.length;
+		return function(){
+			var l = arguments.length,
+				i = -1,
+				def;
+			
+			outer: while(++i < imax){
+				def = definitions[i];
+				if (def.pattern.length !== l) {
+					continue;
+				}
+				var j = -1;
+				while(++j < l){
+					var fn  = def.pattern[j];
+					var val = arguments[j];
+					if (fn(val) === false) {
+						continue outer;
+					}
+				}
+				return def.handler.apply(ctx, arguments);
+			}
+			
+			console.error('InvalidArgumentException for a function', definitions, arguments);
+			return null;
+		};
+	};
+	
+	function pattern_Match(pattern, val){
+		return pattern(val);
+	}
 }());
