94e57abca24377a5daba5d639e984996b0650439
Alex Kit <source.kit@gmail.com>
2016-01-03 00:21:41 +0100

    add (string) dedent

--- src/str.js
+++ src/str.js
@@ -1,4 +1,5 @@
-var str_format;
+var str_format,
+	str_dedent;
 (function(){
 	str_format = function(str_){
 		var str = str_,
@@ -14,7 +15,25 @@ var str_format;
 
 		return str_;
 	};
+	str_dedent = function(str) {
+		var rgx = /^[\t ]*\S/gm,
+			match = rgx.exec(str),
+			count = -1;
+		while(match != null) {			
+			var x = match[0].length;
+			if (count === -1 || x < count) count = x;
+			match = rgx.exec(str);
+		}		
+		if (--count < 1)
+			return str;
 
+		var replacer = new RegExp('^[\\t ]{1,' + count + '}', 'gm');		
+		return str
+			.replace(replacer, '')
+			.replace(/^[\t ]*\r?\n/,'')
+			.replace(/\r?\n[\t ]*$/,'')
+			;
+	};
 	var rgxNum;
 	(function(){
 		rgxNum = function(num){
