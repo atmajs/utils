0d9280cf7c0be6e590e39a43981b9c0dd3e5950e
Alex Kit <source.kit@gmail.com>
2017-07-20 13:44:18 +0200

    add (types) export also each util as symbol

--- types/module.d.ts
+++ types/module.d.ts
@@ -2,3 +2,30 @@
 
 export default Lib;
 
+export const is_Function: typeof Is.is_Function;
+export const is_Array: typeof Is.is_Array
+export const is_ArrayLike: typeof Is.is_ArrayLike
+export const is_String: typeof Is.is_String
+export const is_Object: typeof Is.is_Object
+export const is_notEmptyString: typeof Is.is_notEmptyString
+export const is_rawObject: typeof Is.is_rawObject
+export const is_Date: typeof Is.is_Date
+export const is_NODE: typeof Is.is_NODE
+export const is_DOM: typeof Is.is_DOM
+
+
+export const obj_getProperty: typeof Obj.obj_getProperty;
+export const obj_setProperty: typeof Obj.obj_setProperty;
+export const obj_hasProperty: typeof Obj.obj_hasProperty;
+export const obj_extend: typeof Obj.obj_extend;
+export const obj_extendDefaults: typeof Obj.obj_extendDefaults;
+export const obj_extendMany: typeof Obj.obj_extendMany;
+export const obj_extendProperties: typeof Obj.obj_extendProperties;
+export const obj_extendPropertiesDefaults: typeof Obj.obj_extendPropertiesDefaults;
+export const obj_create: typeof Obj.obj_create;
+export const obj_defineProperty: typeof Obj.obj_defineProperty;
+
+export const class_create: typeof ClassBase.class_create;
+export const class_Dfr: typeof ClassBase.class_Dfr;
+export const class_EventEmitter: typeof ClassBase.class_EventEmitter;
+export const class_Uri: typeof ClassBase.class_Uri;
