fc9f3353ac2eecd262f92986b4a96155bc4d1da5
Alex Kit <source.kit@gmail.com>
2019-01-30 12:07:02 +0100

    bump

--- src/refs.ts
+++ src/refs.ts
@@ -5,3 +5,12 @@ export const _Array_indexOf = Array.prototype.indexOf;
 export const _Object_hasOwnProp = Object.hasOwnProperty;
 export const _Object_getOwnProp = Object.getOwnPropertyDescriptor;
 export const _Object_defineProperty = Object.defineProperty;
+
+declare var global: any;
+declare var window: any;
+
+const $global = typeof global !== 'undefined' ? global : window;
+export { $global as global };
+
+const $document = typeof window !== 'undefined' && window.document != null ? window.document : null;
+export { $document as document };
