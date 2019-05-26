e4d7fe322386aeed2155168081f39cb03770bf61
Alex Kit <source.kit@gmail.com>
2019-05-26 23:06:07 +0200

    bump

--- src/is.ts
+++ src/is.ts
@@ -1,10 +1,10 @@
-export function is_Function(x) {
+export function is_Function(x): x is Function {
     return typeof x === 'function';
 }
-export function is_Object(x) {
+export function is_Object(x): x is any {
     return x != null && typeof x === 'object';
 }
-export function is_Array(arr) {
+export function is_Array<T = any>(arr): arr is T[] {
     return (
         arr != null &&
         typeof arr === 'object' &&
@@ -13,16 +13,16 @@ export function is_Array(arr) {
     );
 }
 export const is_ArrayLike = is_Array;
-export function is_String(x) {
+export function is_String(x): x is string {
     return typeof x === 'string';
 }
 export function is_notEmptyString(x) {
     return typeof x === 'string' && x !== '';
 }
-export function is_rawObject(x) {
+export function is_rawObject(x): x is object{
     return x != null && typeof x === 'object' && x.constructor === Object;
 }
-export function is_Date(x) {
+export function is_Date(x): x is Date{
     if (x == null || typeof x !== 'object') {
         return false;
     }
@@ -31,7 +31,7 @@ export function is_Date(x) {
     }
     return false;
 }
-export function is_PromiseLike (x) {
+export function is_PromiseLike <T = any> (x): x is PromiseLike<T> {
     return x != null && typeof x === 'object' && typeof x.then === 'function';
 }
 export function is_Observable (x) {
