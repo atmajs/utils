abeb807e83b3ae6e1ab3c29583a9f64ecea13d78
Alex Kit <alex.kit@atmajs.com>
2021-05-07 23:13:04 +0200

    publish: add (obj_clean)

--- src/export-lib.ts
+++ src/export-lib.ts
@@ -2,7 +2,20 @@ import { class_create, class_createEx } from './class';
 import { arr_remove, arr_each, arr_indexOf, arr_contains, arr_pushMany } from './arr';
 import { error_createClass } from './error';
 import { fn_createByPattern, fn_doNothing } from './fn';
-import { obj_getProperty, obj_setProperty, obj_hasProperty, obj_extend, obj_extendDefaults, obj_extendMany, obj_extendProperties, obj_extendPropertiesDefaults, obj_create, obj_defineProperty } from './obj';
+import {
+    obj_getProperty,
+    obj_setProperty,
+    obj_hasProperty,
+    obj_extend,
+    obj_extendDefaults,
+    obj_extendMany,
+    obj_extendProperties,
+    obj_extendPropertiesDefaults,
+    obj_create,
+    obj_defineProperty,
+    obj_clean,
+} from './obj';
+
 import { is_Function, is_Array, is_ArrayLike, is_String, is_Object, is_notEmptyString, is_rawObject, is_Date, is_NODE, is_DOM } from './is';
 import { str_format, str_dedent } from './str';
 import { class_Dfr } from './class/Dfr';
@@ -40,6 +53,7 @@ export const Lib = {
     obj_extendPropertiesDefaults,
     obj_create,
     obj_defineProperty,
+    obj_clean,
 
     is_Function,
     is_Array,
--- src/obj.ts
+++ src/obj.ts
@@ -1,9 +1,10 @@
+import { is_ArrayLike, is_Object } from './is';
 import { _Object_defineProperty, _Object_getOwnProp } from './refs';
 
 const getDescriptor = Object.getOwnPropertyDescriptor;
 const defineDescriptor = Object.defineProperty;
 
-let obj_copyProperty = getDescriptor == null 
+let obj_copyProperty = getDescriptor == null
     ? (target, source, key) => target[key] = source[key]
     : (target, source, key) => {
         let descr = getDescriptor(source, key);
@@ -187,11 +188,107 @@ export function obj_defaults(target, defaults) {
 }
 
 
+interface IObjectCleanOpts {
+    removePrivate?: boolean
+    skipProperties?: { [key: string]: any }
+    ignoreProperties?: { [key: string]: any }
+    strictProperties?: { [key: string]: any }
+    deep?: boolean
+    removeEmptyArrays?: boolean
+    removeFalsy?: boolean
+    shouldRemove? (key: string , val: any): boolean
+}
+
+/**
+ * Remove all NULL properties, optionally also all falsy-ies
+ */
+export function obj_clean<T extends any> (json:T, opts: IObjectCleanOpts = {
+    removePrivate: false,
+    skipProperties: null,
+    removeEmptyArrays: false,
+    removeFalsy: false
+}): T {
+    if (json == null || typeof json !== 'object') {
+        return json;
+    }
+
+    if (is_ArrayLike(json)) {
+        let arr = json as any[];
+        let i = 0;
+        let notNullIndex = -1;
+        for (; i < arr.length; i++) {
+            let val = arr[i];
+            if (val != null) {
+                notNullIndex = i;
+            }
+            obj_clean(val, opts);
+        }
+        // clean all last nullable values
+        if (notNullIndex + 1 < arr.length) {
+            arr.splice(notNullIndex + 1);
+        }
+        return json;
+    }
+
+    if (is_Object(json)) {
+        for (let key in json) {
+            if (opts.skipProperties != null && key in opts.skipProperties) {
+                delete json[key];
+                continue;
+            }
+            if (opts.ignoreProperties != null && key in opts.ignoreProperties) {
+                continue;
+            }
+            if (opts.removePrivate === true && key[0] === '_') {
+                delete json[key];
+                continue;
+            }
+            let val = json[key];
+            if (opts.shouldRemove?.(key, val)) {
+                delete json[key];
+                continue;
+            }
+            if (isDefault(val, opts)) {
+                if (opts.strictProperties != null && key in opts.strictProperties && val != null) {
+                    continue;
+                }
+                delete json[key];
+                continue;
+            }
+            if (opts.deep !== false) {
+                obj_clean(val, opts);
+            }
+            if (opts.removeEmptyArrays && is_ArrayLike(val) && val.length === 0) {
+                delete json[key];
+            }
+        }
+        return json;
+    }
+
+    return json;
+}
+
+
+function isDefault (x, opts) {
+    if (x == null) {
+        return true;
+    }
+    if (opts.removeFalsy && (x === '' || x === false)) {
+        return true;
+    }
+    if (opts.removeEmptyArrays && is_ArrayLike(x) && x.length === 0) {
+        return true;
+    }
+    return false;
+}
+
+
+
 let obj_extendDescriptors;
 let obj_extendDescriptorsDefaults;
 
 (function() {
-    
+
     if (getDescriptor == null) {
         obj_extendDescriptors = obj_extend;
         obj_extendDescriptorsDefaults = obj_defaults;
